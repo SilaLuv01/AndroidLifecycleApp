754f1a80412c034f5885ba79f97ee93f0eb91558
SilaLuv01 <npphl8@gmail.com>
2026-09-12 20:50:31 +0700

    feat: add onCreate lifecycle toast

--- app/src/main/java/com/example/androidlifecycle/MainActivity.kt
+++ app/src/main/java/com/example/androidlifecycle/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.androidlifecycle
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
@@ -9,6 +10,12 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Toast.makeText(
+            applicationContext,
+            "onCreate()",
+            Toast.LENGTH_SHORT
+        ).show()
 
         setContent {
             Text("Android Lifecycle")
