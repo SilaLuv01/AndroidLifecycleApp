3b38c207a97c3c7542fd728fd51c046c2f59026f
SilaLuv01 <npphl8@gmail.com>
2026-09-12 21:29:37 +0700

    feat: update student photo compose UI

--- app/src/main/java/com/example/androidlifecycle/MainActivity.kt
+++ app/src/main/java/com/example/androidlifecycle/MainActivity.kt
@@ -4,7 +4,21 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
 
@@ -18,9 +32,12 @@ class MainActivity : ComponentActivity() {
         ).show()
 
         setContent {
-            Text("Android Lifecycle")
+            MaterialTheme {
+                StudentScreen()
+            }
         }
     }
+
     override fun onStart() {
         super.onStart()
 
@@ -30,6 +47,7 @@ class MainActivity : ComponentActivity() {
             Toast.LENGTH_SHORT
         ).show()
     }
+
     override fun onResume() {
         super.onResume()
 
@@ -39,6 +57,7 @@ class MainActivity : ComponentActivity() {
             Toast.LENGTH_SHORT
         ).show()
     }
+
     override fun onPause() {
         super.onPause()
 
@@ -48,6 +67,7 @@ class MainActivity : ComponentActivity() {
             Toast.LENGTH_SHORT
         ).show()
     }
+
     override fun onStop() {
         super.onStop()
 
@@ -57,6 +77,7 @@ class MainActivity : ComponentActivity() {
             Toast.LENGTH_SHORT
         ).show()
     }
+
     override fun onDestroy() {
         super.onDestroy()
 
@@ -65,5 +86,42 @@ class MainActivity : ComponentActivity() {
             "onDestroy()",
             Toast.LENGTH_SHORT
         ).show()
+    }
+}
+
+@Composable
+fun StudentScreen() {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(
+                id = R.drawable.my_photo
+            ),
+            contentDescription = "Student Photo",
+            modifier = Modifier.size(250.dp),
+            contentScale = ContentScale.Crop
+        )
+
+        Spacer(
+            modifier = Modifier.height(20.dp)
+        )
+
+        Text(
+            text = "Android Lifecycle",
+            style = MaterialTheme.typography.headlineSmall
+        )
+
+        Spacer(
+            modifier = Modifier.height(8.dp)
+        )
+
+        Text(
+            text = "ชื่อ-นามสกุลของนักศึกษา"
+        )
     }
 }
